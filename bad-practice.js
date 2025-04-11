d630aaac59ac1b3e1b3614b86e31def224955f95
team-adamstewartai <tricia@digitalbond.com.au>
2025-04-11 16:46:04 +0800

    chore: add bad code to test CodeRabbit

---
+++ bad-practice.js
@@ -0,0 +1,11 @@
+// This file is for testing CodeRabbit's code review
+const password = "123456";
+eval("console.log('This could be dangerous')");
+
+function add(a, b) {
+  return a + b;
+}
+
+if (true == 'true') {
+  console.log("Loose equality check detected");
+}
